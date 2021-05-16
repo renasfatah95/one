4a02af7a0565c24f21b985fc316281d779e1f906
renasfatah95 <83961057+renasfatah95@users.noreply.github.com>
2021-05-16 08:39:51 +0300

    Update payment.php

--- payment.php
+++ payment.php
@@ -5,12 +5,8 @@ use Guzzle\Http\EntityBody;
 use Guzzle\Http\Message\Request;
 use Guzzle\Http\Message\Response;
 use GuzzleHttp\Exception\RequestException;
-
-
 class Payment
-
 {
-
     private $merchantToken = "Basic TUVSQ0hBTlRfUEFZTUVOVF9HQVRFV0FZOk1lcmNoYW50R2F0ZXdheUBBZG1pbiMxMjM="; 
     private $username = "7500077954";  
     private $password = "Nass@2020";    
@@ -23,13 +19,10 @@ class Payment
 
 
     public function __construct()
-    {
-        $this->client = new Client([
+    {$this->client = new Client([
             'base_uri' => 'https://uatgw1.nasswallet.com/payment/transaction/',
             'timeout' => '1000'
-        ]);
-    }
-
+        ]); }
 
     public function loginWithMerchantAccount()
     {     
@@ -38,37 +31,29 @@ class Payment
                 "username" => $this->username,
                 "password" => $this->password,
                 "grantType" => $this->grantType
-            ]
-        ];
-
+            ]];
         $response = json_decode($this->client->request('POST', 'login', [
             "headers" => ['authorization' => "$this->merchantToken"],
             'json' => $payload
             
         ])->getBody());
-
-                
+    
         if ($response->responseCode == 0 && $response->data->access_token) {
             $response =  $response->data->access_token;
             $this->makeTransaction($response);
         } else {
              return $response->message;
-        }
-    }
+        }}
 
-    
-    public function makeTransaction($access_token)
-    {
-        $payload = [
+     public function makeTransaction($access_token)
+    {$payload = [
             'data' => [
                 'userIdentifier' => $this->username,
                 'transactionPin' => $this->transactionPin,
                 'orderId' => $this->orderId,
                 'amount' => $this->amount,
                 'languageCode' => $this->languageCode
-            ]
-        ];
-
+            ]];
         $response = json_decode($this->client->request('POST', 'initTransaction', [
             "headers" => ['authorization' => "Bearer $access_token"],
             "json" => $payload
@@ -76,7 +61,7 @@ class Payment
 
         if ($response->responseCode == 0 && $response->data->transactionId) {
             
-            echo "https://checkout.nasswallet.com/payment-gateway?id={$response->data->transactionId}&token={$response->data->token}&userIdentifier={$this->username}";
+            echo "https://uatcheckout1.nasswallet.com/payment-gateway?id={$response->data->transactionId}&token={$response->data->token}&userIdentifier={$this->username}";
            
         }else {
             return "something went wrong!";
